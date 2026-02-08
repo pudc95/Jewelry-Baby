555873b28a26e6cd9f42a21da476fbf12634e878
pudc95 <42628110+pudc95@users.noreply.github.com>
2026-02-08 13:39:53 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// 第一页 开始 - Hero Section 按钮点击事件
+
+document.getElementById("exploreBtn").addEventListener("click", function() {
+  alert("Explore the Collection button clicked!");
+  // 这里可以跳转到下一个部分或页面
+});
+
+// 第一页 结束
